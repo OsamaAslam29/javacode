4124eec17c5fab1039c06378f59fd70b0e2f8612
Osama <osamaaslam029@gmail.com>
2021-08-27 23:49:04 +0500

    adeel

--- src/com/company/_013_String.java
+++ src/com/company/_013_String.java
@@ -5,15 +5,19 @@ import java.util.Locale;
 public class _013_String {
     public static void main(String[] args) {
         // STRING in Java.
+//
+//        String a, b, d;
+//        a = "Hellow";
+//        b = "alij";
+//        System.out.println(a + " " + b);
+//        d = "My Fav book is Chips";
+//        int nn = a.length();
+//        System.out.println("The Length of A \\n Variable = " + nn);
+//        System.out.println(d.indexOf("book"));
+//        System.out.println(d.toUpperCase());
 
-        String a, b, d;
-        a = "Hellow";
-        b = "alij";
-        System.out.println(a + " " + b);
-        d = "My Fav book is Chips";
-        int nn = a.length();
-        System.out.println("The Length of A \\n Variable = " + nn);
-        System.out.println(d.indexOf("book"));
-        System.out.println(d.toUpperCase());
+        System.out.println("java");
+
+
     }
 }
