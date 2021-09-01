cd52c365b979018dec030c76ca0afb67c354765e
Osama <osamaaslam029@gmail.com>
2021-09-01 22:48:15 +0500

    cpa

--- src/com/company/_11_Exercise1_shortouts.java
+++ src/com/company/_11_Exercise1_shortouts.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class _11_Exercise1_shortouts {
     public static void main(String[] args) {
 
-        float eng,math,phy,urdu,comp,sum;
+        float eng,math,phy,urdu,comp,bio,sum ;
         System.out.println(" Numbers out of  : 100 ");
         System.out.print("enter the no.s of English : ");
         Scanner Eng  = new Scanner(System.in);
@@ -12,6 +12,10 @@ public class _11_Exercise1_shortouts {
         System.out.print("enter the no.s of Math : ");
         Scanner Math = new Scanner(System.in);
         math = Math.nextFloat();
+
+        System.out.print("enter the no.s of bio : ");
+        Scanner Bio = new Scanner(System.in);
+        bio = Bio.nextFloat();
 
         System.out.print("enter the no.s of Phy : ");
         Scanner Phy  = new Scanner(System.in);
@@ -25,7 +29,7 @@ public class _11_Exercise1_shortouts {
         Scanner Comp = new Scanner(System.in);
         comp = Comp.nextFloat();
 
-        sum = (eng + math + phy + urdu + comp)/5 ;
+        sum = (eng + math + phy + urdu + comp+ bio )/6;
         System.out.print("your percentage is : " + sum);
 
     }
