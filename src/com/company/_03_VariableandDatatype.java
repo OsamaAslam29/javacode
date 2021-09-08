c31b9dbacd5a3799ee3712cc7c0a3319006f6b97
Osama <osamaaslam029@gmail.com>
2021-09-08 22:32:47 +0500

    3

--- src/com/company/_03_VariableandDatatype.java
+++ src/com/company/_03_VariableandDatatype.java
@@ -54,10 +54,10 @@ Example: int number = 10; (Here, int is a data type, the number is the variable 
 public class _03_VariableandDatatype {
      public static void main(String[] args) {
         int num1= 10;
-        int num2=66;
+//        int num2=66;
         int num3=33;
-        int sum = num1 +num2 +num3;
+        int sum = num1  +num3;
         System.out.println(sum);
-        System.out.println(sum);
+        
         }
 }
